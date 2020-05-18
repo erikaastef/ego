60d4be098eff9352488dfbb473ca4110c427eb49
Erika Stefania Castillo Barragan <erikaastef99@Erikas-MBP.fibertel.com.ar>
2020-05-18 20:06:11 -0300

    Updates

---
+++ precache-manifest.b05148196b31b998fd894b4c87f8c00a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ee744d42b09848502cc59816349327",
+    "url": "/egoChallenge/index.html"
+  },
+  {
+    "revision": "7cbee8f836b226527c58",
+    "url": "/egoChallenge/static/js/2.a741c0bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/egoChallenge/static/js/2.a741c0bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469f5526f7ea9a0afe62",
+    "url": "/egoChallenge/static/js/main.23ff51c8.chunk.js"
+  },
+  {
+    "revision": "c86797dcbf6b1decbce4",
+    "url": "/egoChallenge/static/js/runtime-main.f2a803bd.js"
+  },
+  {
+    "revision": "9ffccfa886402a8639f62316812abaaa",
+    "url": "/egoChallenge/static/media/C-HR.9ffccfa8.png"
+  },
+  {
+    "revision": "90c557c26901536bed725547b9e4c9c8",
+    "url": "/egoChallenge/static/media/Camry.90c557c2.png"
+  },
+  {
+    "revision": "d5e728de38d30f479628977f97c10664",
+    "url": "/egoChallenge/static/media/Corolla.d5e728de.png"
+  },
+  {
+    "revision": "a83b135714dbe0910f0b8131eb8471c1",
+    "url": "/egoChallenge/static/media/Etios.a83b1357.png"
+  },
+  {
+    "revision": "29bfa43abd0595dbfb3b429764bb6bfa",
+    "url": "/egoChallenge/static/media/Hiace.29bfa43a.png"
+  },
+  {
+    "revision": "473084d5a59b1f58173e6ea5ba04edd4",
+    "url": "/egoChallenge/static/media/Hilux.473084d5.png"
+  },
+  {
+    "revision": "d5bbad5f8745f08540ae3b2ca0295e99",
+    "url": "/egoChallenge/static/media/Hilux2.d5bbad5f.png"
+  },
+  {
+    "revision": "96eed35c547e8ee530cdad1b509617f5",
+    "url": "/egoChallenge/static/media/Prius.96eed35c.png"
+  },
+  {
+    "revision": "2b32d779d274a3b61a31cca3161db332",
+    "url": "/egoChallenge/static/media/RAV4.2b32d779.png"
+  },
+  {
+    "revision": "9c5b70873ee84a15b09f1018fb893925",
+    "url": "/egoChallenge/static/media/SW4.9c5b7087.png"
+  },
+  {
+    "revision": "9d4d73affe0e03e14f8cd7a5018bd142",
+    "url": "/egoChallenge/static/media/Yaris.9d4d73af.png"
+  },
+  {
+    "revision": "57d6536b8e1a6bf32542a732e55c52fc",
+    "url": "/egoChallenge/static/media/airbags.57d6536b.png"
+  },
+  {
+    "revision": "6ce542f1697bee94af41f1e79e575b94",
+    "url": "/egoChallenge/static/media/asientosCuero.6ce542f1.png"
+  },
+  {
+    "revision": "9a583f287b8af1ef2568e5c7556e63df",
+    "url": "/egoChallenge/static/media/cajaAutomatica.9a583f28.png"
+  },
+  {
+    "revision": "e49cf570aaf4d18ea436e1e51336a3c2",
+    "url": "/egoChallenge/static/media/cajaManual.e49cf570.png"
+  },
+  {
+    "revision": "f63067e5b7cbaecbb2fd835bb873d8da",
+    "url": "/egoChallenge/static/media/camaraTrasera.f63067e5.png"
+  },
+  {
+    "revision": "ada3b1eb54868329f2b7c6190beb9bae",
+    "url": "/egoChallenge/static/media/climatizador.ada3b1eb.png"
+  },
+  {
+    "revision": "1cb134a29f6e84cdf113db6968b37dba",
+    "url": "/egoChallenge/static/media/controlVelocidad.1cb134a2.png"
+  },
+  {
+    "revision": "23fbbaed02f8bcf8069e9a64bceede78",
+    "url": "/egoChallenge/static/media/displayDigital.23fbbaed.png"
+  },
+  {
+    "revision": "d423f7251ef4ec3dc0248a5e4f3e7629",
+    "url": "/egoChallenge/static/media/isofix.d423f725.png"
+  }
+]);
