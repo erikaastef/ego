d1670cb88f3f51fac6a23f0a2cd3d8e47269d0fa
Erika Stefania Castillo Barragan <erikaastef99@Erikas-MBP.fibertel.com.ar>
2020-05-18 21:48:55 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EgoChallenge/precache-manifest.ee351c0d0c9a001f692f141f3dfbcb30.js"
+  "/ego/precache-manifest.bc324c01bebf6047384867463c5408bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EgoChallenge/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ego/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
